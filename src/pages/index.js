d88e643ea4e49f1109b9ce5b153be117027066ef
Daniel Balloch <43333774+danielpbdesign@users.noreply.github.com>
2020-01-01 20:19:45 +1300

    basic homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,37 @@
 import React from "react"
 import { Link } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hi I'm Daniel</h1>
+    <p>A front-end developer whose always learning and building.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      <Img fluid={data.placeholderImage.childImageSharp.fluid} />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
 export default IndexPage
+
+
+
+export const Query = graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "headshotmin.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `
